128897c3471804f1d45a71628ac6737231a697cd
Steffen Martinsen <steffen@steffenmedia.no>
2019-05-09 13:24:39 +0200

    Initial creation

---
+++ src/lib/TestComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TestComponent extends Component {
+    render() {
+        return (
+            <div>
+                This is test component
+            </div>
+        );
+    }
+}
+
+export default TestComponent;
